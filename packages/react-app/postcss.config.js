8de55d8656c9e59df5c6a1887bd488ba690d289c
Ryan Berckmans <ryanberckmans@gmail.com>
2022-04-22 15:38:42 -0500

    Fork of react-scripts to use tailwind/nesting
    See https://github.com/ryanberckmans/create-react-app/releases/tag/postcss-tailwind-fix-v0.1

--- packages/react-app/postcss.config.js
+++ packages/react-app/postcss.config.js
@@ -1,8 +1,4 @@
+// WARNING create-react-app doesn't actually use this file. Instead, the react-scripts package will auto-detect the use of tailwind by the existence of tailwind.config.js, and then create-react-app has its own baked-in list of postcss plugins used in the build. To make this work with tailwindcss/nesting, I forked create-react-app and updated this list of plugins to those recommended by tailwind's Adam Wathan https://github.com/ryanberckmans/create-react-app/releases/tag/postcss-tailwind-fix-v0.1
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: {}
 }
