fd64a01d10e54dc43306bd93e299d09b972fc354
Ryan Berckmans <ryanberckmans@gmail.com>
2022-04-27 17:14:22 -0500

    fixup

--- packages/react-app/src/usedappCurrencies.ts
+++ packages/react-app/src/usedappCurrencies.ts
@@ -8,7 +8,7 @@ import { isProduction } from "./isProduction";
 // https://chainlist.org/
 const USDC = new Token('USD Coin', 'USDC', Mainnet.chainId, '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48');
 const KovanUSDC = new Token('USD Coin', 'USDC', Kovan.chainId, '0x50dC5200082d37d5dd34B4b0691f36e3632fE1A8');
-const USDT = new Token('Tether USD', 'USDT', Mainnet.chainId, '0x7F5c764cBc14f9669B88837ca1490cCa17c31607');
+const USDT = new Token('Tether USD', 'USDT', Mainnet.chainId, '0xdac17f958d2ee523a2206206994597c13d831ec7');
 const KovanUSDT = new Token('Tether USD', 'USDT', Kovan.chainId, '0xe0BB0D3DE8c10976511e5030cA403dBf4c25165B');
 
 // Optimism token list (for mainnet and Optimism Kovan https://static.optimism.io/optimism.tokenlist.json)
@@ -18,10 +18,10 @@ const OptimismEther = new NativeCurrency('Ether', 'ETH', Optimism.chainId);
 const OptimismKovanEther = new NativeCurrency('Ether', 'ETH', OptimismKovan.chainId);
 const OptimismDai = new Token('Dai', 'DAI', Optimism.chainId, '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1');
 const OptimismKovanDai = new Token('Dai', 'DAI', OptimismKovan.chainId, '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1');
-const OptimismUSDC = new Token('USD Coin', 'USDC', Optimism.chainId, '0x7F5c764cBc14f9669B88837ca1490cCa17c31607');
+const OptimismUSDC = new Token('USD Coin', 'USDC', Optimism.chainId, '0x7F5c764cBc14f9669B88837ca1490cCa17c31607'); // TODO OptimismUSDC matches the contract address in the explorer, https://optimistic.etherscan.io/token/0x7f5c764cbc14f9669b88837ca1490cca17c31607 but it seems to fail to load based on a quick test with our TokenBalance component returning '?'
 const OptimismKovanUSDC = new Token('USD Coin', 'USDC', OptimismKovan.chainId, '0x4e62882864fB8CE54AFfcAf8D899A286762B011B');
 const OptimismUSDT = new Token('Tether USD', 'USDT', Optimism.chainId, '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58');
-const OptimismKovanUSDT = new Token('Tether USD', 'USDT', OptimismKovan.chainId, '0x7F5c764cBc14f9669B88837ca1490cCa17c31607');
+const OptimismKovanUSDT = new Token('Tether USD', 'USDT', OptimismKovan.chainId, '0x7F5c764cBc14f9669B88837ca1490cCa17c31607'); // TODO WARNING not sure why this addresss is the same as OptimismUSDC but that's how it is defined in the Optimism token list
 
 // Arbitrum mainnet token list: https://bridge.arbitrum.io/token-list-42161.json
 // Arbitrum rinkeby token list: https://bridge.arbitrum.io/token-list-421611.json
