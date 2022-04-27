c1724e1cf686d22843e1cc42a1f77ce98725e481
Ryan Berckmans <ryanberckmans@gmail.com>
2022-04-27 16:58:14 -0500

    Add TokenBalance component
    TokenBalance is an initial starter component to render a user's token
    balance on a particular chain, like "10 DAI on Arbitrum Rinkeby".

---
+++ packages/react-app/src/TokenBalance.tsx
@@ -0,0 +1,52 @@
+import { BigNumber } from "@ethersproject/bignumber";
+import { formatEther } from "@ethersproject/units";
+import { NativeCurrency, Token, useEtherBalance, useTokenBalance } from "@usedapp/core";
+import React from "react";
+import { getChainName } from "./getChainName";
+
+type RenderProps = {
+  balance: BigNumber | undefined;
+  ticker: string;
+  chainName: string;
+}
+const Render: React.FC<RenderProps> = ({ balance, ticker, chainName }) => {
+  return <span>{balance ? formatEther(balance) : '?'} {ticker} on {chainName}</span>;
+}
+
+type NativeCurrencyBalanceProps = {
+  nativeCurrency: NativeCurrency;
+  address: string;
+};
+const NativeCurrencyBalance: React.FC<NativeCurrencyBalanceProps> = ({ nativeCurrency, address }) => {
+  const b = useEtherBalance(address, { chainId: nativeCurrency.chainId });
+  return <Render balance={b} ticker={nativeCurrency.ticker} chainName={getChainName(nativeCurrency.chainId)} />;
+}
+
+
+type TokenBalanceProps = {
+  token: Token;
+  address: string;
+};
+const TokenBalance: React.FC<TokenBalanceProps> = ({ token, address }) => {
+  const b = useTokenBalance(token.address, address, { chainId: token.chainId });
+  console.log("useTokenBalance", token, address, b);
+  return <Render balance={b} ticker={token.ticker} chainName={getChainName(token.chainId)
+  } />;
+}
+
+function isToken(o: NativeCurrency | Token): o is Token {
+  return Object.prototype.hasOwnProperty.call(o, "address");
+}
+
+// TODO s/Wrapper/TokenBalance/ and rename the other TokenBalance to InternalTokenBalance or something
+type WrapperProps = {
+  nativeCurrencyOrToken: NativeCurrency | Token; // native currency or token whose address balance will be fetched and rendered. TODO maybe aggregate NativeCurrency/Token into a wrapper type 3cities.Token
+  address: string; // address whose token balance will be loaded and rendered
+};
+const Wrapper: React.FC<WrapperProps> = ({ nativeCurrencyOrToken, address }) => {
+  return isToken(nativeCurrencyOrToken) ?
+    <TokenBalance token={nativeCurrencyOrToken} address={address} /> :
+    <NativeCurrencyBalance nativeCurrency={nativeCurrencyOrToken} address={address} />;
+}
+
+export default Wrapper;
