95b449350e91493227d82b74a13284c3e22bfa2b
Ryan Berckmans <ryanberckmans@gmail.com>
2022-05-01 13:03:32 -0500

    fixup

--- packages/react-app/src/hooks/useAddressOrENS.ts
+++ packages/react-app/src/hooks/useAddressOrENS.ts
@@ -1,9 +1,9 @@
 import { useEthers, useLookupAddress } from "@usedapp/core";
 
-// useAddressOrENS returns the currently connected wallet address's
+// useConnectedWalletAddressOrENS returns the currently connected wallet address's
 // primary ENS name, or else the address itself if no primary ENS name
 // is set, or else undefined if no address is connected.
-export default function useAddressOrENS(): string | undefined {
+export default function useConnectedWalletAddressOrENS(): string | undefined {
     const { account } = useEthers();
     const ens = useLookupAddress();
     if (typeof account !== 'string') return undefined;
