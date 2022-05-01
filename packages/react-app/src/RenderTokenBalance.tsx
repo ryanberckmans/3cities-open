275a8019e5f38065df028d0959092214fa26b1c4
Ryan Berckmans <ryanberckmans@gmail.com>
2022-05-01 09:56:38 -0500

    Add RenderTokenBalance

---
+++ packages/react-app/src/RenderTokenBalance.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { getChainName } from "./getChainName";
+import { RenderRawTokenBalance } from "./RenderRawTokenBalance";
+import { TokenBalance } from "./tokenBalance";
+import { getTokenByTokenKey } from "./usedappCurrencies";
+
+type RenderTokenBalanceProps = {
+  tokenBalance: TokenBalance;
+}
+
+// RenderTokenBalance is a referentially transparent component to
+// render the passed TokenBalance.
+export const RenderTokenBalance: React.FC<RenderTokenBalanceProps> = ({ tokenBalance }) => {
+  const t = getTokenByTokenKey(tokenBalance.tokenKey);
+  return <RenderRawTokenBalance
+    balance={tokenBalance.balanceAsBigNumberHexString}
+    ticker={t.ticker}
+    chainName={getChainName(t.chainId)}
+  />;
+}
