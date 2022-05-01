55fc0190e3db29c65407829ee4be1adf57c825d8
Ryan Berckmans <ryanberckmans@gmail.com>
2022-05-01 11:43:05 -0500

    Add ZkSyncTestnet to usedappConfig

--- packages/react-app/src/usedappConfig.ts
+++ packages/react-app/src/usedappConfig.ts
@@ -1,4 +1,4 @@
-import { Arbitrum, ArbitrumRinkeby, Config, Kovan, Mainnet, Optimism, OptimismKovan } from "@usedapp/core";
+import { Arbitrum, ArbitrumRinkeby, Config, Kovan, Mainnet, Optimism, OptimismKovan, ZkSyncTestnet } from "@usedapp/core";
 import { isProduction } from "./isProduction";
 
 const INFURA_PROJECT_ID = "defba93b47f748f09fcead8282b9e58e"; // TODO get our own Infura id, this is the one that ships with create-eth-app
@@ -16,5 +16,6 @@ export const config: Config = isProduction ? {
     [Kovan.chainId]: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
     [OptimismKovan.chainId]: 'https://kovan.optimism.io',
     [ArbitrumRinkeby.chainId]: 'https://rinkeby.arbitrum.io/rpc',
+    [ZkSyncTestnet.chainId]: 'https://zksync2-testnet.zksync.dev',
   },
 };
