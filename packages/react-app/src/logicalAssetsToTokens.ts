5f722e6fb2bf812981bed8d2b235cd48062e4492
Ryan Berckmans <ryanberckmans@gmail.com>
2022-05-04 11:23:11 -0500

    Add logicalAssetsToTokens.ts

---
+++ packages/react-app/src/logicalAssetsToTokens.ts
@@ -0,0 +1,14 @@
+import { NativeCurrency, Token } from "@usedapp/core";
+import { logicalAssetsByTicker, LogicalAssetTicker } from "./logicalAssets";
+import { tokensByTicker } from "./tokens";
+
+export function getNativeCurrenciesAndTokensForLogicalAssetTicker(lat: LogicalAssetTicker): (NativeCurrency | Token)[] {
+  const la = logicalAssetsByTicker[lat];
+  const r: (NativeCurrency | Token)[] = [];
+  for (const t of Object.keys(la.supportedTokenTickers)) {
+    const maybeTokens = tokensByTicker[t];
+    if (maybeTokens !== undefined) r.push(...maybeTokens);
+  }
+  console.log("getNativeCurrenciesAndTokensForLogicalAssetTicker lat=", lat, "r=", r);
+  return r;
+}
