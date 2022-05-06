b57b462b70bf35bac4d97f72b680e19b38ec4539
Ryan Berckmans <ryanberckmans@gmail.com>
2022-05-06 14:09:13 -0500

    Add tech demo to App

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -1,15 +1,34 @@
-import { abis, addresses } from "@3cities/contracts";
-import { useQuery } from "@apollo/client";
-import { Contract } from "@ethersproject/contracts";
-import { shortenAddress, useCall, useEthers, useLookupAddress } from "@usedapp/core";
+import { shortenAddress, useEthers, useLookupAddress } from "@usedapp/core";
 import React, { useEffect, useState } from "react";
+import { acceptReceiverProposedPayment, isReceiverProposedPayment } from "./agreements";
+import { Checkout } from "./checkout";
 import { Body, Container, Header } from "./components";
-import GET_TRANSFERS from "./graphql/subgraph";
-import useAddressOrENS from "./hooks/useAddressOrENS";
+import { useConnectedWalletAddressContext } from './connectedWalletContextProvider';
+import { parseLogicalAssetAmount } from "./logicalAssets";
+import { RenderProposedStrategy, RenderStrategy } from "./RenderStrategy";
+import { RenderTokenBalance } from "./RenderTokenBalance";
+import { getProposedStrategiesForProposedAgreement, getStrategiesForAgreement } from "./strategies";
+import { allTokenKeys, getTokenKey } from "./tokens";
+// import { useConnectedWalletAddressContext } from "./connectedWalletContextProvider";
+
+// Old imports:
+// import { abis, addresses } from "@3cities/contracts";
+// import { useQuery } from "@apollo/client";
+// import { Contract } from "@ethersproject/contracts";
+// import { formatEther } from '@ethersproject/units';
+// import { shortenAddress, useCall, useEtherBalance, useEthers, useLookupAddress, useTokenBalance } from "@usedapp/core";
+// import React, { useEffect, useState } from "react";
+// import { Body, Container, Header } from "./components";
+// import GET_TRANSFERS from "./graphql/subgraph";
+// import useAddressOrENS from "./hooks/useAddressOrENS";
+// import { nativeCurrencies, tokens } from "./usedappCurrencies";
+// import TokenBalance from "./TokenBalance";
+// import { useConnectedWalletAddressContext } from "./connectedWalletContextProvider";
 
 function WalletButton() {
   const [rendered, setRendered] = useState("");
 
+  // TODO there is a "bug" in useLookAddress, or maybe a feature depending on your perspective, where it attempts to look up the ENS address using the wallet's active network. This means that if your address has a primary ENS name set on mainnet, it won't be properly detected and loaded unless your wallet network is set to mainnet. But we want to make an app that always works the same way regardless of which network your wallet is connected to --> implement our own useEnsAddress(address) hook which always looks up the reverse address on mainnet if isProduction, and if !isProduction always look up reverse address on some canonical testnet implementation of ENS (it looks like ENS is deployed on Ropsten, Rinkeby and Goerli https://docs.ens.domains/ens-deployments)
   const ens = useLookupAddress();
   const { account, activateBrowserWallet, deactivate, error } = useEthers();
 
@@ -45,37 +64,131 @@ function WalletButton() {
   );
 }
 
+const Leaf: React.FC = () => {
+  const ac = useConnectedWalletAddressContext();
+  // console.log("Leaf render", ac);
+  let tokensRendered = 0;
+  if (ac === undefined) return null;
+  else return <div>
+    connected address: {ac.address}<br />
+    {allTokenKeys.map(tk => {
+      const tb = ac.tokenBalances[tk];
+      if (tb === undefined) return;
+      else {
+        tokensRendered += 1;
+        return <div key={tk}>
+          <RenderTokenBalance tokenBalance={tb} />
+        </div>;
+      }
+    })}
+    {tokensRendered < 1 && "(no token balances)"}
+  </div>;
+}
+
+const Intermediate: React.FC = () => {
+  // const [num, setNum] = useState(0);
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     setNum(n => n + 1);
+  //   }, 10000);
+  //   return () => clearInterval(intervalId);
+  // }, [setNum]);
+  // console.log("Intermediate render", num);
+  return <div>
+    {/*num % 2 === 0 &&*/ <Leaf /> /* here we flicker Leaf in and out of existence to test subscription clearing on unmount */}
+  </div>;
+}
+
+const testCheckout: Checkout = {
+  proposedAgreement: {
+    toAddress: '0xac0d7753EA2816501b57fae9ad665739018384b3',
+    logicalAssetTicker: 'USD',
+    amountAsBigNumberHexString: parseLogicalAssetAmount("30.12").toHexString(),
+    _p: false,
+    _rpp: true,
+  },
+  strategyPreferences: {
+    tokenTickerExclusions: ['USDC'],
+    chainIdExclusions: [42],
+  },
+};
+
+function ProposedStrategyTest() {
+  const pss = getProposedStrategiesForProposedAgreement(testCheckout.strategyPreferences, testCheckout.proposedAgreement);
+  if (pss.length < 1) return <div>(no proposed strategies)</div>;
+  else return <div>
+    proposed strategies:
+    {pss.map(ps => <div key={getTokenKey(ps.receiverProposedTokenTransfer.token)}><RenderProposedStrategy ps={ps} /></div>)}
+  </div >;
+}
+
+function StrategyTest() {
+  const ac = useConnectedWalletAddressContext();
+  if (ac === undefined) return <div>no strategies because no wallet is connected</div>;
+  else if (isReceiverProposedPayment(testCheckout.proposedAgreement)) {
+    const ss = getStrategiesForAgreement(testCheckout.strategyPreferences, acceptReceiverProposedPayment(ac.address, testCheckout.proposedAgreement), ac);
+    if (ss.length < 1) return <div>(no strategies)</div>;
+    else return <div>
+      strategies:
+      {ss.map(s => <div key={getTokenKey(s.tokenTransfer.token)}>
+        <RenderStrategy s={s} />
+      </div>)}
+    </div >;
+  } else return <div>strategy test: unsupported agreement type</div>;
+}
+
 function App() {
-  const maybeAddressOrENS = useAddressOrENS();
+  // const maybeAddressOrENS = useAddressOrENS();
+  // const firstNativeCurrencyBalance = useEtherBalance(maybeAddressOrENS, { chainId: nativeCurrencies[0].chainId });
+  // const firstTokenBalance = useTokenBalance(tokens[0].address, maybeAddressOrENS, { chainId: tokens[0].chainId });
+  // console.log('addr', maybeAddressOrENS, 'firstTokenBalance', firstTokenBalance);
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // const ftb = useMemo(() => firstTokenBalance, [firstTokenBalance && firstTokenBalance.toString()]);
 
   // Read more about useDapp on https://usedapp.io/
-  const { error: contractCallError, value: tokenBalance } =
-    useCall({
-      contract: new Contract(addresses.ceaErc20, abis.erc20),
-      method: "balanceOf",
-      args: ["0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C"],
-    }) ?? {};
-  console.log(contractCallError, tokenBalance);
+  // const { error: contractCallError, value: tokenBalance } =
+  //   useCall({
+  //     contract: new Contract(addresses.ceaErc20, abis.erc20),
+  //     method: "balanceOf",
+  //     args: ["0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C"],
+  //   }) ?? {};
+  // console.log(contractCallError, tokenBalance);
 
-  const { loading, error: subgraphQueryError, data } = useQuery(GET_TRANSFERS);
+  // const { loading, error: subgraphQueryError, data } = useQuery(GET_TRANSFERS);
 
-  useEffect(() => {
-    if (subgraphQueryError) {
-      console.error("Error while querying subgraph:", subgraphQueryError.message);
-      return;
-    }
-    if (!loading && data && data.transfers) {
-      console.log({ transfers: data.transfers });
-    }
-  }, [loading, subgraphQueryError, data]);
+  // useEffect(() => {
+  //   if (subgraphQueryError) {
+  //     console.error("Error while querying subgraph:", subgraphQueryError.message);
+  //     return;
+  //   }
+  //   if (!loading && data && data.transfers) {
+  //     console.log({ transfers: data.transfers });
+  //   }
+  // }, [loading, subgraphQueryError, data]);
+
+  // const STAKING_CONTRACT = '0x00000000219ab540356cBB839Cbe05303d7705Fa';
+  // const stakingBalance = useEtherBalance(STAKING_CONTRACT);
+  // console.log('stakingBalance', stakingBalance && formatEther(stakingBalance));
 
   return (
     <Container>
       <Header>
         <WalletButton />
-        {maybeAddressOrENS && <><br/>connected: {maybeAddressOrENS}</>}
+        {/* {ftb && <><br />first token balance: {formatEther(ftb)}</>} */}
+        {/* {maybeAddressOrENS && <><br />connected: {maybeAddressOrENS}</>}
+        {firstNativeCurrencyBalance && <><br />first native currency balance: {formatEther(firstNativeCurrencyBalance)}</>}
+        {firstTokenBalance && <><br />first token balance: {formatEther(firstTokenBalance)}</>}
+        {maybeAddressOrENS && nativeCurrencies.map(nc => <div key={`${nc.ticker} ${nc.chainId}`}>
+          <TokenBalance nativeCurrencyOrToken={nc} address={maybeAddressOrENS} key={`${nc.ticker} ${nc.chainId}`} />
+        </div>)}
+        {maybeAddressOrENS && tokens.map(nc => <div key={`${nc.ticker} ${nc.chainId}`}>
+          <TokenBalance nativeCurrencyOrToken={nc} address={maybeAddressOrENS} key={`${nc.ticker} ${nc.chainId}`} />
+        </div>)} */}
       </Header>
       <Body>
+        <Intermediate />
+        <ProposedStrategyTest />
+        <StrategyTest />
       </Body>
     </Container>
   );
