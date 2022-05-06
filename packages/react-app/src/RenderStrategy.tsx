3b9097f65f7578d46af97fdeeca0dcc6b9b5ae53
Ryan Berckmans <ryanberckmans@gmail.com>
2022-05-06 13:52:55 -0500

    Add RenderStrategy

--- packages/react-app/src/RenderStrategy.tsx
+++ packages/react-app/src/RenderStrategy.tsx
@@ -2,14 +2,26 @@ import { formatUnits } from "@ethersproject/units";
 import React from "react";
 import { formatFloat } from "./formatFloat";
 import { getChainName } from "./getChainName";
-import { ProposedStrategy } from "./strategies";
+import { ProposedStrategy, Strategy } from "./strategies";
 import { getDecimalsToRenderForTokenTicker } from "./tokens";
+
+type RenderStrategyProps = {
+  s: Strategy;
+}
+
+// RenderStrategy is a referentially transparent component to render
+// the passed strategy.
+export const RenderStrategy: React.FC<RenderStrategyProps> = ({ s }) => {
+  const t = s.tokenTransfer.token;
+  return <span>Pay {formatFloat(formatUnits(s.tokenTransfer.amountAsBigNumberHexString, t.decimals), getDecimalsToRenderForTokenTicker(t.ticker))} {t.ticker} on {getChainName(t.chainId)
+  }</span>
+}
 
 type RenderProposedStrategyProps = {
   ps: ProposedStrategy;
 }
 
-// RenderProposedStrategy is a referentially transparent componet to
+// RenderProposedStrategy is a referentially transparent component to
 // render the passed proposed strategy.
 export const RenderProposedStrategy: React.FC<RenderProposedStrategyProps> = ({ ps }) => {
   const t = ps.receiverProposedTokenTransfer.token;
