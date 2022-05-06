25683a1e33bb64681db9f00602fb664bb9f6f32c
Ryan Berckmans <ryanberckmans@gmail.com>
2022-05-06 12:43:33 -0500

    Support decimal counts for logical assets

--- packages/react-app/src/logicalAssets.ts
+++ packages/react-app/src/logicalAssets.ts
@@ -1,3 +1,5 @@
+import { BigNumber } from "@ethersproject/bignumber";
+import { parseUnits } from "@ethersproject/units";
 
 // Design note: we have designed logical assets to have no dependecies (or at least, no dependency on tokens) so that the infrastructure for building and computing data structures over logical assets is separate from and doesn't depend on our supported crypto tokens or chains. Below, we implicitly connect logical assets to crypto tokens by providing a list of supported crypto token tickers for each logical asset. Note that these crypto token tickers don't actually have to exist and be supported by our system; if they don't exist, they'll simply be ignored silently at runtime.
 
@@ -44,3 +46,37 @@ export const logicalAssetsByTicker: Readonly<{ [key in LogicalAssetTicker]: Logi
 };
 
 export const logicalAssets: Readonly<LogicalAsset[]> = Object.values(logicalAssetsByTicker);
+
+const logicalAssetDecimals = 6; // all logical assets have 6 decimals, ie. we model their amounts as if they were tokens with 6 decimals. For example, a logical asset amount for $5.75 is `5.75 * 10^6`. This allows us to map logical asset amounts to popular token amounts with no loss in precision because USDC and USDT have 6 decimals (requiring no widening) and ETH and DAI have 18 decimals (logical assets can be widened with no loss in precision by multiplying the logical asset amount by 10^12). However, this modeling means that logical assets can't express the full precision of ETH and DAI, eg. logical assets can't express the precision of wei or gwei
+
+// parseLogicalAssetAmount takes a string representation of a float
+// amount, such as "5.75" and returns the logical asset integer amount
+// based on the number of token decimals used for logical assets.
+export function parseLogicalAssetAmount(amount: string): BigNumber {
+  return parseUnits(amount, logicalAssetDecimals);
+}
+
+const ten: BigNumber = BigNumber.from(10);
+
+// convertLogicalAssetUnits converts the passed logicalAssetAmount
+// (which is assumed to have logicalAssetDecimals decimal places
+// because it was constructed with parseLogicalAssetAmount) into the
+// passed newDecimals count of decimal places. For example,
+// `convertLogicalAssetUnits(myLogicalAssetAmount, DAI.decimals)`
+// converts the passed logical asset amount into a DAI amount.
+export function convertLogicalAssetUnits(logicalAssetAmount: BigNumber, newDecimals: number): BigNumber {
+  if (newDecimals < logicalAssetDecimals) throw new Error(`convertLogicalAssetUnits: unsupported narrowing of passed logical asset amount to newDecimals=${newDecimals}`); // TODO support narrowing
+  return logicalAssetAmount.mul(ten.pow(newDecimals - logicalAssetDecimals));
+}
+
+// getDecimalsToRenderForTLogicalAsseticker returns the canonical
+// number of digits after the decimal point to render for a logical
+// asset based on its passed ticker.
+export function getDecimalsToRenderForLogicalAssetTicker(ticker: LogicalAssetTicker): number {
+  switch (ticker) {
+    case 'ETH': return 4;
+    case 'USD': return 2;
+    case 'CAD': return 2;
+    case 'EUR': return 2;
+  }
+}
