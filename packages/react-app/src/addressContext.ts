17f145c894b2b04a7b1482764f4b1f4f574cb595
Ryan Berckmans <ryanberckmans@gmail.com>
2022-05-06 13:53:24 -0500

    Add canAfford
    A predicate to determine if an address can afford a token payment.

--- packages/react-app/src/addressContext.ts
+++ packages/react-app/src/addressContext.ts
@@ -1,3 +1,4 @@
+import { BigNumber } from "@ethersproject/bignumber";
 import { TokenBalance } from "./tokenBalance";
 import { TokenKey } from "./tokens";
 
@@ -13,4 +14,13 @@ export function emptyAddressContext(address: string): AddressContext {
     address,
     tokenBalances: {},
   };
+}
+
+// canAfford is a convenience predicate that returns true iff the
+// passed address context can afford to transfer the passed amount of
+// the token indicated by the passed token key.
+export function canAfford(ac: AddressContext, tk: TokenKey, amountAsBigNumberHexString: string): boolean {
+  const tb = ac.tokenBalances[tk];
+  if (tb === undefined) return false;
+  else return BigNumber.from(tb.balanceAsBigNumberHexString).gte(BigNumber.from(amountAsBigNumberHexString)); // NB here we are able to compare nominal amounts directly without worrying about units because the passed amount is denominated in the same token as the tokenbalance because both are for the passed token key
 }
